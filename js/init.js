39bd168a61664402d9258d8964f284ba9d8088db
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2017-04-03 11:19:29 +0530

    BrownBread Bakery
    BrownBread Bakery

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+      $('.slider').slider();
+
+        
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
